75ffac6123258521cf61da5a32d859c669d6532b
C Hema latha <c.hema11024@gmail.com>
2023-06-20 20:23:55 +0530

    question1,2,3,4,5 are committed

---
+++ Module1/Day4/question1.c
@@ -0,0 +1,18 @@
+/*1.Sum and average of 1D array*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i,sum,avg;
+    int arr[] = {12,84,20,36,64,56,72,10,45,90};
+    for(i=0;i<10;i++){
+        sum = sum + arr[i];
+        printf("The Sum of %d\n elements is %d\n",i,sum);
+    }
+    avg = sum / 10;
+    printf("Average of Elements in 1D array is : %d",avg);
+
+    return 0;
+}
