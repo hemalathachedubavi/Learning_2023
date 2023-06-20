77f4b845408230537f3df8be5bf586a1a952a0a1
C Hema latha <c.hema11024@gmail.com>
2023-06-20 22:51:36 +0530

    question1,2,3,4,5 are committed

--- Module1/Day5/question1.c
+++ Module1/Day5/question1.c
@@ -1,24 +1,52 @@
-/*1.Toggle case*/
+/* 1. Use structures to find the volume and the total surface area of the box. 
+   You need to access the members of the structure with the help of a structure pointer:
+    a. With the help of (*) asterisk or indirection operator and (.) dot operator.
+    b. With the help of ( -> ) Arrow operator using pointer as well as dot representation.*/
 
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+    #include <stdio.h>
+
+struct Box {
+    double length;
+    double width;
+    double height;
+};
+
+double calculateVolume(struct Box* boxPtr) {
+    return boxPtr->length * boxPtr->width * boxPtr->height;
+}
+
+double calculateSurfaceArea(struct Box* boxPtr) {
+    return 2 * (boxPtr->length * boxPtr->width + boxPtr->length * boxPtr->height + boxPtr->width * boxPtr->height);
+}
 
 int main() {
-    char str[] = { 'A','b','c','d',' ','X','Y','Z','\0'};
-    int size = sizeof(str) / sizeof(str[0]);
-
-    printf("Original String: %s\n", str);
-
-    for (int i = 0; i < size; i++) {
-        if (islower(str[i])) {
-            str[i] = toupper(str[i]);
-        } else if (isupper(str[i])) {
-            str[i] = tolower(str[i]);
-        }
-    }
-
-    printf("Toggle String is : %s\n", str);
-
+    struct Box myBox;
+    struct Box* boxPtr = &myBox;
+    
+    // Assign values to the box members
+    myBox.length = 5.0;
+    myBox.width = 3.0;
+    myBox.height = 4.0;
+    
+    // Access structure members using (*) asterisk and (.) dot operator
+    printf("Using (*) and (.) operators:\n");
+    printf("Length: %.2f\n", (*boxPtr).length);
+    printf("Width: %.2f\n", (*boxPtr).width);
+    printf("Height: %.2f\n", (*boxPtr).height);
+    
+    // Access structure members using (->) arrow operator
+    printf("\nUsing (->) operator:\n");
+    printf("Length: %.2f\n", boxPtr->length);
+    printf("Width: %.2f\n", boxPtr->width);
+    printf("Height: %.2f\n", boxPtr->height);
+    
+    // Calculate volume and surface area
+    double volume = calculateVolume(boxPtr);
+    double surfaceArea = calculateSurfaceArea(boxPtr);
+    
+    printf("\nVolume: %.2f\n", volume);
+    printf("Surface Area: %.2f\n", surfaceArea);
+    
     return 0;
 }
+
