3338a480833fe965f9b2c1779393c43f64ad80fc
C Hema latha <c.hema11024@gmail.com>
2023-06-22 18:22:38 +0530

    question1,2,3,4,5,6,7,8 are committed

--- Module1/Day4/question1.c
+++ Module1/Day4/question1.c
@@ -5,14 +5,15 @@
 
 int main()
 {
-    int i,sum,avg;
+    int i,sum=0;
+    float avg;
     int arr[] = {12,84,20,36,64,56,72,10,45,90};
     for(i=0;i<10;i++){
         sum = sum + arr[i];
         printf("The Sum of %d\n elements is %d\n",i,sum);
     }
     avg = sum / 10;
-    printf("Average of Elements in 1D array is : %d",avg);
+    printf("Average of Elements in 1D array is : %f",avg);
 
     return 0;
 }
